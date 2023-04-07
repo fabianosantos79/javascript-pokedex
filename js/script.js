ddba1d800076532369fda665cdf97eaab01cfecc
Fabiano <fabiano.ssantos79@gmail.com>
2023-04-07 01:17:09 -0300

    update system

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-let apiPokemon = `https://pokeapi.co/api/v2/pokemon/ditto`;
+const pokemonNumber = document.querySelector('.pokemon_number')
+const pokemonName = document.querySelector('.pokemon_name')
+const pokemonImage = document.querySelector('.pokemon_image')
+const form = document.querySelector('.form')
+const input = document.querySelector('.input_search')
+
+const getPokemon = async (pokemon) =>{
+    const apiPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+    const dataPokemon = await apiPokemon.json();
+
+    pokemonNumber.innerHTML = dataPokemon.id
+    pokemonName.innerHTML = dataPokemon.name
+    pokemonImage.src = dataPokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault
+
+    getPokemon(input.value)
+})
