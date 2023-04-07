f8b70762393bff30c724e28041b78364e7d5b400
Fabiano <fabiano.ssantos79@gmail.com>
2023-04-07 03:31:32 -0300

    system finally

--- js/script.js
+++ js/script.js
@@ -1,21 +1,60 @@
 const pokemonNumber = document.querySelector('.pokemon_number')
 const pokemonName = document.querySelector('.pokemon_name')
 const pokemonImage = document.querySelector('.pokemon_image')
+
 const form = document.querySelector('.form')
 const input = document.querySelector('.input_search')
+const btnNext = document.querySelector('.btn-next')
+const btnPrev = document.querySelector('.btn-prev')
+let pokemonPosition = 1
 
 const getPokemon = async (pokemon) =>{
+    pokemonName.innerHTML = 'Loading...'
+    pokemonNumber.innerHTML = ''
     const apiPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
-    const dataPokemon = await apiPokemon.json();
+    if(apiPokemon.status === 200){
+        const data = await apiPokemon.json();
+        return data;
+    }
+}
 
-    pokemonNumber.innerHTML = dataPokemon.id
-    pokemonName.innerHTML = dataPokemon.name
-    pokemonImage.src = dataPokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+const renderPokemon = async (pokemon) => {
+
+    const dataPokemon = await getPokemon(pokemon)
+
+    if(dataPokemon){
+        pokemonPosition = dataPokemon.id
+        pokemonImage.style.display = 'block'
+        pokemonNumber.innerHTML = `${dataPokemon.id} - `
+        pokemonName.innerHTML = dataPokemon.name
+        pokemonImage.src = dataPokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        input.value = ''
+    
+    }else{
+        pokemonName.innerHTML = 'Not found.';
+        pokemonNumber.innerHTML = '';
+        input.value = '';
+        pokemonImage.style.display = 'none';
+    }
+
 }
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault
-
-    getPokemon(input.value)
+    event.preventDefault()
+    renderPokemon(input.value.toLowerCase())
 })
+
+btnNext.addEventListener('click', () => {
+    pokemonPosition += 1; 
+    renderPokemon(pokemonPosition)
+})
+
+btnPrev.addEventListener('click', () => {
+    if(pokemonPosition > 1){
+    pokemonPosition -= 1
+    renderPokemon(pokemonPosition)
+    }
+})
+
+renderPokemon(pokemonPosition)
